9dbc14a2ca952d2b7bfcc65d8bed3ed751f96b96
yangyao <yangyao@yangyaodeMacBook-Pro.local>
2015-09-06 22:33:52 +0800

    Initial Commit

---
+++ tableviews/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  tableviews
+//
+//  Created by yangyao on 15/9/6.
+//  Copyright (c) 2015年 yangyao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
